8ea19127e8e07bc96d5393d718528d8763823c12
MichelTsukiyama <michel.tsukiyama@gmail.com>
2021-06-29 00:14:25 -0300

    Ferramentas de controle

--- controle.js/if.js
+++ controle.js/if.js
@@ -27,3 +27,21 @@ seForVerdadeEuFalo([])
 seForVerdadeEuFalo([1,2])
 seForVerdadeEuFalo({})
 //true^^
+
+function teste1(num) {
+    if (num > 7) //JS executa somente uma sentença sem usar bloco '{}'
+        console.log(num)
+        console.log('Final') //é executado sem nenhuma relação com a condição
+}
+
+teste1(6)
+teste1(8)
+
+function teste2(num){
+    if(num > 7); { // o ';' está sendo uma sentença de código vazia, e o console.log não faz parte da condição
+        console.log(num)
+    }
+}
+
+teste2(6)
+teste2(8)
