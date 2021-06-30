1707c3e93bdd4efa36687098f927faecbccebe6b
MichelTsukiyama <michel.tsukiyama@gmail.com>
2021-06-29 23:51:47 -0300

    Update ifElse.js
    ainda meio em dúvida no Number.prototype.entre e tentar jogar uma string...

--- controle.js/ifElse.js
+++ controle.js/ifElse.js
@@ -11,26 +11,27 @@ imprimirResultado(4)
 imprimirResultado('Epa!!')*/ //fracamente tipado, recebe a string mas não dá para avaliar se é >= 7, sendo o resultado 'false' 'Reprovado!'
 
 Number.prototype.entre = function (inicio, fim) {
-    return this >= inicio && this <= fim
+    return this >= inicio && this <= fim 
 }
 
 const imprimirResultado = function (nota) {
     if (nota.entre(9, 10)){
         console.log('Quadro de honra')
     } else if (nota.entre(7, 8.99)) {
-        console.log('Aprovado!')
+        console.log('Aprovado')
     } else if (nota.entre(4, 6.99)){
-        console.log('Recuperação!')
+        console.log('Recuperação')
     } else if (nota.entre(0, 3.99)){
-        console.log('Reprovado!')
-    } else {
-        console.log ('Nota inválida')
+        console.log('Reprovado')
+    } else { 
+        console.log('Nota inválida')
     }
     console.log('fim')
+
 }
 
 imprimirResultado(9.5)
 imprimirResultado(7)
 imprimirResultado(6.99)
 imprimirResultado(2)
-imprimirResultado('AA')
+//imprimirResultado('AA') não aceitou a string, diz que 'entre' não foi definido. Ainda não entendi direito o por quê disso.
